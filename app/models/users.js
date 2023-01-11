ed394bb310b4c4019596b7c1045eee4f227336ef
lolettalee <lolettalee@hotmail.co.uk>
2023-01-11 21:21:44 +0000

    edit app.js, users.js, database

--- app/models/users.js
+++ app/models/users.js
@@ -15,41 +15,49 @@ class Users {
 
   // Methods for registering and logging in
   // Check if submitted email address exists in user table 
-  async getIdFromEmail() {
-    var sql = "SELECT user_id FROM user WHERE user_email = ?";
-    const result = await db.query(sql, [this.email]);
-    this.email = result.user_email;
-    // TODO LOTS OF ERROR CHECKS HERE..
-    if (JSON.stringify(result) != '[]') {
-        this.id = result[0].id;
-        return this.id;
-    }
-    else {
-        return false;
-    }
+async getIdFromEmail() {
+  var sql = "SELECT user_id FROM user WHERE user.user_email = ?";
+  const result = await db.query(sql, [this.email]);
+  // TODO LOTS OF ERROR CHECKS HERE..
+  if (JSON.stringify(result) != '[]') {
+      this.id = result[0].id;
+      return this.id;
+  }
+  else {
+      return false;
+  }
 }
 
   // Add a password to an existing user
   async setUserPassword(password) {
     const pw = await bcrypt.hash(password, 10);
-    var sql = "UPDATE user SET password = ? WHERE user_id = ?"
+    var sql = "UPDATE user SET user_password = ? WHERE user.user_id = ?"
     const result = await db.query(sql, [pw, this.id]);
-    return true;
-  }
+    console.log("this.id");
+    console.log(this.id);
+    console.log("pw");
+    console.log(pw);
+    console.log("password");
+    console.log(password);
+    return pw;
+}
 
   // Add a new record to the users table   
   async addUser(password) {
     const pw = await bcrypt.hash(password, 10);
-    var sql = "INSERT INTO user (user_email, password) VALUES (? , ?)";
-    const result = await db.query(sql, [this.email, pw]);
+    var sql = "INSERT INTO user (user_name, user_email, user_password) VALUES (?, ? , ?)";
+    const result = await db.query(sql, [this.name, this.email, pw]);
+    console.log("addUser");
+    console.log(this.name);
     console.log(result.insertId);
     this.id = result.insertId;
     return true;
-  }
+}
 
   // Test a submitted password against a stored password (authenticate method)
   async authenticate(submitted) {
-    var sql = "SELECT password FROM user WHERE user_id = ?";
+    // Get the stored, hashed password for the user
+    var sql = "SELECT user_password FROM user WHERE user.user_id = ?";
     const result = await db.query(sql, [this.id]);
     const match = await bcrypt.compare(submitted, result[0].password);
     if (match == true) {
@@ -60,7 +68,7 @@ class Users {
     }
 }
 
-  }
+}
 
 module.exports = {
   Users
